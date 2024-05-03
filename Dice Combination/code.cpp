691836e30c6d07799cbc2c9df68d6579c7f3ced5
Tarun <92327755+TarunNehla@users.noreply.github.com>
2024-05-03 13:15:51 +0530

    Update code.cpp

--- Dice Combination/code.cpp
+++ Dice Combination/code.cpp
@@ -14,11 +14,9 @@ int n; cin >> n;
 vector<int> dp(n + 1);
 dp[0] = 1;
 
-for(int i = 1; i <= n; i++){
-    for(int j = 1; j <= 6; j++){
-        if(j <= i){
-            dp[i] = (dp[i] + dp[i - j]) % mod;
-        }
+for(int i = 1; i <7; i++){
+    for(int j = 1; j <= n; j++){
+        dp[j] += dp[j-i];
     }
 } 
 
